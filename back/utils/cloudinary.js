dd2b71c9dbe40c9f39a08e09070944abcf2bbaee
1129 <M11307009@gapps.knu.edu.tw>
2024-11-29 15:51:27 +0800

    new branch

--- back/utils/cloudinary.js
+++ back/utils/cloudinary.js
@@ -1,8 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-    cloud_name: "dpmp34kwp",
-    api_key: "",
-    api_secret: "dlR0eSwfqQ_oHhuiqe9RGkdc-n4",
+    cloud_name: process.env.CLOADINARY_CLOUD_NAME,
+    api_key: process.env.CLOADINARY_API_KEY,
+    api_secret: process.env.CLOADINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
