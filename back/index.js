a8c04c3f77c321233fc6605f95c2f8f7b8a1a555
shan-00 <M11307009@gapps.knu.edu.tw>
2024-12-06 07:42:28 +0000

    test

--- back/index.js
+++ back/index.js
@@ -1,7 +1,7 @@
 const express =require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("doten");
+const dotenv = require("dotenv");
 const cors = require("cors"); //cross origin resources sharing
 
 dotenv.config();
